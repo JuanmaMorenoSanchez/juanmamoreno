cbbeb6f7141ce775301ba5e8fd9c96332b48b77c
juanmanuel.moreno <juanmanuel.moreno@nexplore.com>
2024-02-14 17:07:13 +0100

    Gg url. Functional contact

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,5 +6,5 @@ export const environment = {
   polygonNetwork: Network.MATIC_MAINNET,
   homeTokenId: "2",
   adminAdress: "0xD7D089B7eBecCaf4FF8f183F22376913ce6193B9",
-  backendUrl: "http://localhost:3000/"
+  backendUrl: "https://juanmamoreno-backend-oidvzp4jza-no.a.run.app/"
 };
