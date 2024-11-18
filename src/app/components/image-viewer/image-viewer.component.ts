09bbb84013b7638118dac27ffb170bda9d1c6095
juanmanuel.moreno <juanmanuel.moreno@nexplore.com>
2024-11-18 17:23:16 +0100

    Built in viewer

--- src/app/components/image-viewer/image-viewer.component.ts
+++ src/app/components/image-viewer/image-viewer.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
+import { Component, HostBinding, input, Input, OnChanges, Signal, SimpleChanges, ViewChild } from '@angular/core';
 import { NftsService } from '@services/nfts.service';
 import { Nft } from 'alchemy-sdk';
 import { GalleryComponent } from 'ng-gallery';
@@ -9,33 +9,45 @@ import { Lightbox } from 'ng-gallery/lightbox';
   templateUrl: './image-viewer.component.html',
   styleUrls: ['./image-viewer.component.scss']
 })
-export class ImageViewerComponent implements OnChanges {
+export class ImageViewerComponent {
 
-  readonly loadingStrategy = 'lazy';
-  readonly galleryId = 'gallery';
-
-  @Input() nfts!: Array<Nft> | null;
-  
-  @ViewChild(GalleryComponent) gallery!: GalleryComponent;
+  nfts = input<Nft[]>([]);
+  displayIndex = 0;
+  hovering = false;
+  displayArrows = false;
 
   constructor(
     public lightbox: Lightbox,
     private nftService: NftsService,
-  ) {}
+  ) {
+  }
 
   ngOnChanges(changes: SimpleChanges) {
-    this.gallery?.reset();
-    this.addNftsToGallery(changes['nfts'].currentValue as Array<Nft>);
+    this.displayArrows = changes['nfts'].currentValue.length > 1;
   }
 
-  ngAfterViewInit() {
-    this.nfts && this.addNftsToGallery(this.nfts);
+  public setHover(hovering: boolean) {
+    this.hovering = hovering;
   }
 
-
-  private addNftsToGallery(nfts: Array<Nft>): void {
-    nfts.forEach(nft => {
-      this.gallery?.addImage({ src: this.nftService.getQualityUrl(nft.image), thumb: this.nftService.getOptimalUrl(nft.image) });
-    })
+  public nextNft(relativeIndex: number) {
+    this.displayIndex = (this.displayIndex + relativeIndex + this.nfts().length) % this.nfts().length;  
   }
+
+  public getQualityImg(nft: Nft): string {
+    return this.nftService.getQualityUrl(nft?.image);
+  }
+
+  public getSmallImg(nft: Nft): string {
+    return this.nftService.getOptimalUrl(nft?.image);
+  }
+
+  public isFrontalView(nft: Nft): boolean {
+    return this.nftService.isFrontalView(nft);
+  }
+
+  // private findFrontalView(nfts: Nft[]): Nft {
+  //   return nfts.find(nft => this.nftService.isFrontalView(nft) || this.nfts()[0])!
+  // }
+
 }
