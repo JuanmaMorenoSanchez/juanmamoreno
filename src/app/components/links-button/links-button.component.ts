fdab72c3c72c246dcbbf6af2938877fa2094e38c
User <morenosanchezjuanma@gmail.com>
2025-03-19 12:08:56 +0100

    fixes bug on links modal when changing artwork. Uses nxResource

--- src/app/components/links-button/links-button.component.ts
+++ src/app/components/links-button/links-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, input, OnInit } from '@angular/core';
+import { Component, effect, inject, input, OnInit, resource, ResourceRef, signal, WritableSignal } from '@angular/core';
 import { NftsService } from '@services/nfts.service';
 import { MatDialog } from '@angular/material/dialog';
 import { LinksModalComponent } from '@components/links-modal/links-modal.component';
@@ -6,6 +6,8 @@ import { MatIconButton } from '@angular/material/button';
 import { MatTooltip } from '@angular/material/tooltip';
 import { MatIcon } from '@angular/material/icon';
 import { TranslatePipe } from '@ngx-translate/core';
+import { catchError, distinctUntilChanged, map, of } from 'rxjs';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 
 @Component({
@@ -14,27 +16,37 @@ import { TranslatePipe } from '@ngx-translate/core';
     styleUrl: './links-button.component.scss',
     imports: [MatIconButton, MatTooltip, MatIcon, TranslatePipe]
 })
-export class LinksButtonComponent implements OnInit {
+export class LinksButtonComponent {
   private nftsService = inject(NftsService);
   private dialog = inject(MatDialog);
 
   public tokenId = input<string>();
-  public haveLinks = false;
-  private urls: Array<string> | undefined;
+  public haveLinks: WritableSignal<boolean> = signal(false);
+
+  // still an experimental feature in angular v19
+  // not sure if i need this here. It fixes problem with subscriptions not finishing and behaving weird, but it also wrapes data in a weird format that i dont care about
+  private rxUrls = rxResource({
+    request: () => this.tokenId(),
+    loader: ({ request }) =>
+      this.nftsService.getLinks(request as string).pipe(
+        distinctUntilChanged(),
+        map((urls) => urls || []),
+        catchError(() => of([])
+      )
+    )
+  });
 
   constructor( ) {
-  }
-
-  ngOnInit() {
-    this.nftsService.getLinks(this.tokenId()!).subscribe((res: any) => {
-      this.urls = res?.urls;
-      this.haveLinks = !!this.urls?.length;
-    })
+    effect(() => {
+      const urlsValue: any = this.rxUrls.value()!; // not elegant, but works. rxResource wrapps the data in a object with extra metadata (ex: loading status)
+      this.haveLinks.set(!!urlsValue?.urls.length);
+    });
   }
 
   public openLinksModal() {
+    const urlsValue: any = this.rxUrls.value()!;
     this.dialog.open(LinksModalComponent, {
-      data: { links: this.urls }
+      data: { links: urlsValue?.urls}
     });
   }
 }
