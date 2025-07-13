7c630dbbf8aaaca2baf111a057c14027ae613d8b
User <morenosanchezjuanma@gmail.com>
2025-07-13 19:06:34 +0200

    Auto-generated commit

---
+++ chunk-TUSGGUUO.js
@@ -0,0 +1,1 @@
+var e={production:!1,homeTokenId:"2",adminAdress:"0xD7D089B7eBecCaf4FF8f183F22376913ce6193B9",backendUrl:"https://juanmamoreno-backend-164035848667.europe-southwest1.run.app/",backendUrlFallback:"https://juanmamoreno-backend-164035848667.europe-southwest1.run.app/"};export{e as a};
